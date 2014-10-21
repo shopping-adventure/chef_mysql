d8cfeae43726b74bc53b0b3434645ef0443507ec
Kevin <kevin@kbrwadventure.com>
2014-10-21 11:18:41 +0200

    change apt_keyserver with a working one

--- attributes/_percona_repo.rb
+++ attributes/_percona_repo.rb
@@ -19,4 +19,4 @@
 
 default['mysql']['percona']['apt_key_id'] = 'CD2EFD2A'
 default['mysql']['percona']['apt_uri'] = 'http://repo.percona.com/apt'
-default['mysql']['percona']['apt_keyserver'] = 'keys.gnupg.net'
+default['mysql']['percona']['apt_keyserver'] = 'pgp.mit.edu'
